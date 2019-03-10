fb647e9d17f10b112922fb1bd4dc2ca553e97289
Ryan Tate <ryan.michael.tate@gmail.com>
2019-03-09 19:24:26 -0800

    update rsvp handler

--- src/components/rsvp.js
+++ src/components/rsvp.js
@@ -1,19 +1,21 @@
 import React, { Component } from "react";
-import { Link } from "gatsby";
-import { ButtonGroup, Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
-
-
+import { Button, Form, FormGroup, Label, Input } from "reactstrap";
+import axios from "axios";
 
 export default class RSVPForm extends Component {
     constructor() {
         super();
         this.state = {
-            contactName: "",
+            rsvpd: false,
+            going: false,
+            email: "",
+            contact_name: "",
             note: "",
-            partySize: 1,
+            number_guests: 1,
             activities: [
                 {
-                    title: "Lunch, Paint & Sip",
+                    name: "Lunch, Paint & Sip",
+                    title: "paint_sip",
                     checked: false,
                     location: "Uncorked Canvas - Tacoma, WA",
                     capacity: 30,
@@ -21,7 +23,8 @@ export default class RSVPForm extends Component {
                     hour: 16
                 },
                 {
-                    title: "Aerial Yoga",
+                    name: "Aerial Yoga",
+                    title: "aerial_yoga",
                     checked: false,
                     location: "Cilly Fitness at Good Karma - Tacoma, WA",
                     capacity: 10,
@@ -29,23 +32,33 @@ export default class RSVPForm extends Component {
                     hour: 10
                 },
                 {
-                    title: "NW Adventure Trek",
+                    name: "Seattle Adventure",
+                    title: "seattle_adventure",
                     checked: false,
-                    location: "Meet at R&K's House - Tacoma, WA",
-                    capacity: 100,
-                    date: new Date("8/13/2019"),
+                    location: "Meet at R&Ks - Tacoma, WA",
+                    capacity: 10,
+                    date: new Date("8/8/2019"),
                     hour: 8
                 },
+                // {
+                //     title: "NW Adventure Trek",
+                //     checked: false,
+                //     location: "Meet at R&K's House - Tacoma, WA",
+                //     capacity: 100,
+                //     date: new Date("8/13/2019"),
+                //     hour: 8
+                // },
+                // {
+                //     title: "Day Trip to Seattle",
+                //     checked: false,
+                //     location: "Meet at R&K's House - Tacoma, WA",
+                //     capacity: 100,
+                //     date: new Date("8/13/2019"),
+                //     hour: 8
+                // },
                 {
-                    title: "Day Trip to Seattle",
-                    checked: false,
-                    location: "Meet at R&K's House - Tacoma, WA",
-                    capacity: 100,
-                    date: new Date("8/13/2019"),
-                    hour: 8
-                },
-                {
-                    title: "Emerald Downs Horse Races",
+                    name: "Emerald Downs Horse Races",
+                    title: "horse_races",
                     checked: false,
                     location: "Emerald Downs - Auburn, WA",
                     capacity: 100,
@@ -53,7 +66,8 @@ export default class RSVPForm extends Component {
                     hour: 10
                 },
                 {
-                    title: "Tacoma Adventure",
+                    name: "Tacoma Adventure",
+                    title: "tacoma_adventure",
                     checked: false,
                     location: "Meet at R&K's House - Tacoma, WA",
                     capacity: 100,
@@ -61,180 +75,277 @@ export default class RSVPForm extends Component {
                     hour: 10
                 },
                 {
-                    title: "Sounders FC 2 vs. Orange County FC",
+                    name: "Sounders FC 2 vs. Orange County FC",
+                    title: "soccer_game",
                     checked: false,
                     location: "Cheney Stadium - Tacoma, WA",
                     capacity: 100,
                     date: new Date("8/9/2019"),
                     hour: 18
-                },
-                {
-                    title: "Rainer's Minor League Baseball Game",
-                    checked: false,
-                    location: "Cheney Stadium - Tacoma, WA",
-                    capacity: 100,
-                    date: new Date("8/15/2019"),
-                    hour: 18
                 }
+                // {
+                //     title: "Rainer's Minor League Baseball Game",
+                //     checked: false,
+                //     location: "Cheney Stadium - Tacoma, WA",
+                //     capacity: 100,
+                //     date: new Date("8/15/2019"),
+                //     hour: 18
+                // }
             ]
-        }
+        };
     }
-
 
     renderButtons(opts) {
-      return opts.map((opt, i) => {
-        const { ref, title, color, onClick, rsvp, disabled } = opt;
-        return (
-          <div style={{
-            gridColumn: "6",
-            padding: 5
-          }} key={i}>
-            <Button block
-                color={color}
-                onClick={onClick.bind(this, ref, rsvp)}
-                disabled={disabled}
+        return opts.map((opt, i) => {
+            const { ref, title, color, onClick, rsvp, disabled } = opt;
+            return (
+                <div
+                    style={{
+                        gridColumn: "6",
+                        padding: 5
+                    }}
+                    key={i}
                 >
-                    {title}
-            </Button>
-          </div>
-        );
-      });
+                    <Button
+                        block
+                        color={color}
+                        onClick={onClick.bind(this, ref, rsvp)}
+                        disabled={disabled}
+                    >
+                        {title}
+                    </Button>
+                </div>
+            );
+        });
     }
 
-    handleRSVP(ref, rsvp) {
-        console.log('this.state', this.state);
+    async handleRSVP(ref, rsvp) {
+        try {
+            console.log("ref, rsvp", ref, rsvp);
+            console.log("this.state", this.state);
+            await axios.post("http://localhost:3000/wedding/rsvp", this.state);
+
+            this.setState({ rsvpd: true, going: rsvp });
+        } catch (error) {
+            console.log("error", error);
+        }
     }
 
     partySizeOptions(size) {
         const options = [];
         for (var i = 0; i < size; i++) {
-            options.push(
-                <option key={i}>{i+1}</option>
-            )
+            options.push(<option key={i}>{i + 1}</option>);
         }
         return options;
     }
 
     guestPartyInfo() {
-        const { partySize } = this.state;
+        const { number_guests } = this.state;
 
-        if (partySize == 1) {
+        if (number_guests === 1) {
             return null;
         } else {
             const guestParties = [];
-            for (var i = 1; i < partySize; i++) {
+            for (var i = 1; i < number_guests; i++) {
                 guestParties.push(
                     <FormGroup key={i}>
-                      <Label for={`${i}_contactGuest`}>Guest {i} Information</Label>
-                      <Input type="name"
-                            name={`${i}_contactGuest`}
-                            id={`${i}_contactGuest`}
+                        <Label for={`guest_name_${i}`}>
+                            Guest {i} Information
+                        </Label>
+                        <Input
+                            type="name"
+                            name={`guest_name_${i}`}
+                            id={`guest_name_${i}`}
                             placeholder="Enter Your Guest's First and Last Name"
                             onChange={this.handleOnChange.bind(this)}
-                            value={this.state[`${i}_contactGuest`] || ''}
+                            value={this.state[`guest_name_${i}`] || ""}
                         />
                     </FormGroup>
-                )
+                );
             }
             return guestParties;
         }
-
     }
 
-
     activityOptions(activities) {
-        return activities.sort((a, b) => {
-            return a.date - b.date;
-        }).map((activity, i) => {
-            const { title, location, capacity, date, hour, checked } = activity
-            return (
-                <FormGroup check key={i}>
-                  <Label check>
-                    <Input type="checkbox" name={`activity_${i}`} checked={checked} onChange={(e) => {
-                        activities[i] = {
-                            ...activities[i],
-                            checked: e.target.checked
-                        };
-                        this.setState({ activities: activities });
-                    }}/>{' '}
-                    <span style={{ color: '#f0ad4e' }}>{ title }</span> | <span style={{ color: '#4BBF73' }}>{ location }</span> | { date ? date.toLocaleDateString() : null } { date ? new Date(date.setHours(hour)).toLocaleTimeString() : null }
-                  </Label>
-                </FormGroup>
-            )
-        })
-
+        return activities
+            .sort((a, b) => {
+                return a.date - b.date;
+            })
+            .map((activity, i) => {
+                const { name, location, date, hour, checked } = activity;
+                return (
+                    <FormGroup check key={i}>
+                        <Label check>
+                            <Input
+                                type="checkbox"
+                                name={`activity_${i}`}
+                                checked={checked}
+                                onChange={e => {
+                                    activities[i] = {
+                                        ...activities[i],
+                                        checked: e.target.checked
+                                    };
+                                    this.setState({ activities: activities });
+                                }}
+                            />{" "}
+                            <span style={{ color: "#f0ad4e" }}>{name}</span> |{" "}
+                            <span style={{ color: "#4BBF73" }}>
+                                {location}
+                            </span>{" "}
+                            | {date ? date.toLocaleDateString() : null}{" "}
+                            {date
+                                ? new Date(
+                                      date.setHours(hour)
+                                  ).toLocaleTimeString()
+                                : null}
+                        </Label>
+                    </FormGroup>
+                );
+            });
     }
 
     handleOnChange(e) {
         this.setState({
             [e.target.id]: e.target.value
-        })
+        });
+    }
+
+    renderRsvpForm() {
+        const { activities, contact_name } = this.state;
+
+        return (
+            <div
+                style={{
+                    backgroundColor: "rgba(0, 0, 0, 0.5)",
+                    height: 1200,
+                    margin: 50,
+                    padding: 100,
+                    zoom: "75%"
+                }}
+            >
+                <Form
+                    onSubmit={e => {
+                        e.preventDefault();
+                    }}
+                >
+                    <FormGroup>
+                        <Label for="contact_name">Name</Label>
+                        <Input
+                            type="name"
+                            name="name"
+                            id="contact_name"
+                            placeholder="Enter Your First and Last Name"
+                            onChange={this.handleOnChange.bind(this)}
+                            value={this.state["contact_name"]}
+                        />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="email">Email</Label>
+                        <Input
+                            type="email"
+                            name="email"
+                            id="email"
+                            placeholder="Enter your email address"
+                            onChange={this.handleOnChange.bind(this)}
+                            value={this.state["email"]}
+                        />
+                    </FormGroup>
+                    <hr />
+                    <FormGroup>
+                        <Label for="NumberGuests">
+                            Party Size (Incl. You )
+                        </Label>
+                        <Input
+                            type="select"
+                            name="number_guests"
+                            id="NumberGuests"
+                            onChange={e => {
+                                // Set our party size to the changed value
+                                this.setState({
+                                    number_guests: +e.target.value
+                                });
+                            }}
+                        >
+                            {this.partySizeOptions(6)}
+                        </Input>
+                    </FormGroup>
+                    {this.guestPartyInfo()}
+                    <hr />
+                    <FormGroup>
+                        <Label for="note">
+                            Leave a Special Note for Ryan & Kelsey
+                        </Label>
+                        <Input
+                            type="textarea"
+                            name="note"
+                            id="note"
+                            value={this.state["note"]}
+                            onChange={this.handleOnChange.bind(this)}
+                        />
+                    </FormGroup>
+                    <hr />
+                    <FormGroup tag="fieldset">
+                        <legend>Activities To Do While in Town!</legend>
+                        {this.activityOptions(activities)}
+                    </FormGroup>
+                    <hr />
+                    {this.renderButtons([
+                        {
+                            title: "Excitedly Accept!",
+                            ref: "/rsvp/accept",
+                            onClick: this.handleRSVP,
+                            rsvp: true,
+                            disabled: contact_name.length < 5,
+                            color: "success"
+                        },
+                        {
+                            title: "Regretfully Decline!",
+                            ref: "/rsvp/decline",
+                            onClick: this.handleRSVP,
+                            rsvp: false,
+                            disabled: contact_name.length < 5,
+                            color: "secondary"
+                        }
+                    ])}
+                </Form>
+            </div>
+        );
     }
 
     render() {
-        const { activities, contactName } = this.state;
+        const { rsvpd, going } = this.state;
 
-        return (
-            <div style={{ backgroundColor: "rgba(0, 0, 0, 0.5)", margin: 50, padding: 100 }}>
-                <Form onSubmit={(e) => {
-                    e.preventDefault()
-                }}>
-                 <FormGroup>
-                   <Label for="contactName">Name</Label>
-                   <Input
-                        type="name"
-                        name="name"
-                        id="contactName"
-                        placeholder="Enter Your First and Last Name"
-                        onChange={this.handleOnChange.bind(this)}
-                        value={this.state['contactName']}
-                    />
-                 </FormGroup>
-                 < hr/>
-                 <FormGroup>
-                   <Label for="PartySize">Party Size (Incl. You
-                   )</Label>
-                   <Input type="select" name="partysize" id="PartySize" onChange={(e) => {
-                       // Set our party size to the changed value
-                       this.setState({ partySize: +e.target.value })
-                   }}>
-                     { this.partySizeOptions(6) }
-                   </Input>
-                 </FormGroup>
-                 { this.guestPartyInfo() }
-                 < hr/>
-                 <FormGroup>
-                   <Label for="note">Leave a Special Note for Ryan & Kelsey</Label>
-                   <Input type="textarea" name="note" id="note" value={this.state['note']} onChange={this.handleOnChange.bind(this)} />
-                 </FormGroup>
-                 < hr/>
-                 <FormGroup tag="fieldset">
-                   <legend>Activities To Do While in Town!</legend>
-                   { this.activityOptions(activities) }
-                 </FormGroup>
-                 < hr/>
-                 {
-                     this.renderButtons([
-                         {
-                             title: "Excitedly Accept!",
-                             ref: "/rsvp/accept",
-                             onClick: this.handleRSVP,
-                             rsvp: true,
-                             disabled: contactName.length < 5,
-                             color: "success"
-                         },
-                         {
-                             title: "Regretfully Decline!",
-                             ref: "/rsvp/decline",
-                             onClick: this.handleRSVP,
-                             rsvp: false,
-                             disabled: contactName.length < 5,
-                             color: "secondary"
-                         }
-                     ])
-                 }
-               </Form>
-            </div>
-        )
+        return rsvpd ? (
+            going ? (
+                <div style={{ margin: 200, height: 400, zoom: "75%" }}>
+                    <div
+                        style={{
+                            backgroundColor: "rgba(0, 0, 0, 0.5)",
+                            padding: 100,
+                            height: 50,
+                            textAlign: "center"
+                        }}
+                    >
+                        <h1 style={{ color: "white" }}>See you soon!</h1>
+                    </div>
+                </div>
+            ) : (
+                <div style={{ margin: 200, height: 400, zoom: "75%" }}>
+                    <div
+                        style={{
+                            backgroundColor: "rgba(0, 0, 0, 0.5)",
+                            padding: 100,
+                            height: 50,
+                            textAlign: "center"
+                        }}
+                    >
+                        <h1 style={{ color: "white" }}>Sorry to miss you!</h1>
+                    </div>
+                </div>
+            )
+        ) : (
+            this.renderRsvpForm()
+        );
     }
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "gatsby";
 import Header from "../components/header.js";
 import "../styles/bootstrap.min.css";
 
@@ -9,7 +8,7 @@ export default () => (
     <div style={{ backgroundColor: "#1e2d44" }}>
         <Header
             title={"Ryan & Kelsey Tate"}
-            subtitle={"Wedding\nAugust 10th 2019"}
+            subtitle={"Wedding | RSVP | August 10th, 2019"}
             img={headerImg}
         />
     </div>
